a5f965d2cf17a3c3dcd26c51dfaf40458f97c88a
Silvia Zou <18zous32@millshighsch.org>
2016-11-07 08:36:11 -0800

    this is bad

--- SilviaZouFall2016/src/MinMax.java
+++ SilviaZouFall2016/src/MinMax.java
@@ -1,24 +1,26 @@
-//APCS Silvia Zou 10/2/16
-
 import java.util.*;
 public class MinMax {
-	public static void findMaxMin(){
+	public static void main(String[] args){
 		int max = 1; 
 		int min = 1;
 		int counter = 0;
+		System.out.println("Enter a number: ");
+		Scanner initialInput = new Scanner(System.in);
 		for(int i = 0; i <= 10; i++){
-			Scanner initialInput = new Scanner(System.in);
 			int userInput = initialInput.nextInt();
+			System.out.println("Enter another number: ");
 			if(userInput> max){
 				max = userInput;
 			}else if(userInput < min ){
 				min = userInput;
 			}
 			if(userInput % 2 == 0){
-				counter += userInput;	
+				counter += userInput;
 			}
-			System.out.println(counter + min + max);
-			initialInput.close();
 		}
+		System.out.println("The total of the numbers divisible by two is " + counter);
+		System.out.println("The max number is " + max);
+		System.out.println("The min number is " + min);
+		initialInput.close();
 	}
 }
